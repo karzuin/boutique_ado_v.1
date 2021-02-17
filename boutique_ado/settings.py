34481d3b037a3c979140944445be344afd24651f
karzuin <karzuin@yahoo.com>
2021-02-17 15:58:46 +0000

    Removed secret key and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&bi=s0ns#kkwy@n01gp5@0o!a^6#)ddh#r%l7)r8zc()@pi0i#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['karzuin-boutique-ado.herokuapp.com', 'localhost']
 
